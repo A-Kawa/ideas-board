e2d2955a4525609c739cd05a7b4f8e2c66973d8c
Andrzej <kawus9@gmail.com>
2019-02-01 21:51:06 +0200

    Added header styles and name

--- src/containerComponents/Header.jsx
+++ src/containerComponents/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import "../styles/Header.scss";
 
 export const Header = () => {
   return (
     <div>
-      <h1>There will be header!</h1>
+      <h1 className="header-name">Idea Board</h1>
     </div>
   );
 };
