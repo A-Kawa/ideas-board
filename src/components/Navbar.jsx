143b3c840748e9355912e31990d3b4ac96b4eb61
Andrzej <kawus9@gmail.com>
2019-02-05 13:37:22 +0200

    Added buttons to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import { CategorySelector } from "./CategorySelector";
+import { Button } from "semantic-ui-react";
 
 export const Navbar = () => {
-  return <CategorySelector />;
+  return (
+    <div>
+      <CategorySelector />
+      <Button icon="save" content="Save All" />
+      <Button icon="plus circle" content="Add" />
+    </div>
+  );
 };
