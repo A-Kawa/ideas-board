38defae2392b294a912a23c7bdc521a552e1a927
Andrzej <kawus9@gmail.com>
2019-02-05 16:58:58 +0200

    Delete spinner from Home.jsx - it was for tests

--- src/containerComponents/Home.jsx
+++ src/containerComponents/Home.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { Header } from "./Header";
-import { LoaderSpinner } from "../components/LoaderSpinner";
 
 export const Home = () => {
   return (
     <div>
       <Header />
       <h1>Home Page</h1>
-      <LoaderSpinner />
     </div>
   );
 };
