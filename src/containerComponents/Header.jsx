70083dc6b3a8a873121e8b597543b917db30e0f7
Andrzej <kawus9@gmail.com>
2019-02-05 12:56:51 +0200

    Added texts.js to hold texts in one place

--- src/containerComponents/Header.jsx
+++ src/containerComponents/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { headerName } from "../const/texts";
 import "../styles/Header.scss";
 
 export const Header = () => {
   return (
     <div>
-      <h1 className="header-name">Idea Board</h1>
+      <h1 className="header-name">{headerName}</h1>
     </div>
   );
 };
